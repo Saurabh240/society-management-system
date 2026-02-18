38bddd1d49c666e7c7944b40bb361b1a2e4313a0
Sahilmaliya88 <sahilmaliya55@gmail.com>
2026-02-18 23:47:07 +0530

    feat: Community entity and repository

--- backend/src/main/java/com/gstech/saas/communication/community/model/Community.java
+++ backend/src/main/java/com/gstech/saas/communication/community/model/Community.java
@@ -1,4 +1,26 @@
 package com.gstech.saas.communication.community.model;
 
-public class Community {
+import com.gstech.saas.platform.common.BaseEntity;
+import com.gstech.saas.platform.tenant.model.Tenant;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Table(name = "communities")
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Community extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String status;
+
+    @Column(name = "tenant_id")
+    private Long tenantId;
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 }
--- backend/src/main/java/com/gstech/saas/communication/community/repository/CommunityRepository.java
+++ backend/src/main/java/com/gstech/saas/communication/community/repository/CommunityRepository.java
@@ -1,4 +1,18 @@
 package com.gstech.saas.communication.community.repository;
 
-public class CommunityRepository {
+import com.gstech.saas.communication.community.model.Community;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommunityRepository extends JpaRepository<Community,Long> {
+    /**
+     * Get all communities for a tenant
+     */
+    List<Community> findByTenantId(Long tenantID);
+
+    /**
+     * Check if community name already exists for tenant
+     */
+    boolean existsByTenantIdAndName(Long tenantId, String name);
 }
