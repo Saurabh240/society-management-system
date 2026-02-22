bac30d68f7fe610c50f8a6db325e5d87a11ba95e
Sahil Maliya <sahilmaliya55@gmail.com>
2026-02-22 21:26:07 +0530

    Update remove constructor parameters validations

--- backend/src/main/java/com/gstech/saas/communication/property/dtos/PropertyUpdateRequest.java
+++ backend/src/main/java/com/gstech/saas/communication/property/dtos/PropertyUpdateRequest.java
@@ -6,12 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 public record PropertyUpdateRequest(
         @Schema(description = "Property name", example = "Property 1") @NotBlank(message = "Property name is required") String name) {
     public PropertyUpdateRequest {
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("Property name is required");
-        }
-        // if (communityId == null) {
-        // throw new IllegalArgumentException("Community id is required");
-        // }
+        
     }
 
 }
