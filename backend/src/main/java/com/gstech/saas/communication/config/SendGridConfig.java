63e8aa3db34654f3c6bd918b465eb46c33fcd876
Saurabh240 <saurabh58833@gmail.com>
2026-03-23 15:19:18 +0530

    removed sendgrid

--- backend/src/main/java/com/gstech/saas/communication/config/SendGridConfig.java
+++ backend/src/main/java/com/gstech/saas/communication/config/SendGridConfig.java
@@ -1,18 +1,20 @@
-//package com.gstech.saas.communication.config;
-//
-//import com.sendgrid.SendGrid;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SendGridConfig {
-//
-//    @Value("${communication.sendgrid.api-key}")
-//    private String apiKey;
-//
-//    @Bean
-//    public SendGrid sendGrid() {
-//        return new SendGrid(apiKey);
-//    }
-//}
+/*
+package com.gstech.saas.communication.config;
+
+import com.sendgrid.SendGrid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SendGridConfig {
+
+    @Value("${communication.sendgrid.api-key}")
+    private String apiKey;
+
+    @Bean
+    public SendGrid sendGrid() {
+        return new SendGrid(apiKey);
+    }
+}
+*/
